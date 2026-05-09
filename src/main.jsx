44ed1f599fc82db9c4a58c0efbafc553fabdb3fe
Avazbek Odilov <avazbek@gmail.com>
2026-05-09 23:55:53 +0500

    fix cardcontext import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { CardProvider } from './context/CardContext'  // <-- Shu qator
+import { CardProvider } from './context/CardContext.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
