3972e31d732a031f2f9fb1eb7830454ae3a40ebf
Avazbek Odilov <avazbek@gmail.com>
2026-05-09 23:51:09 +0500

    fix import path

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { CardProvider } from './context/CardContext';
+import { CardProvider } from './context/Cardcontext';
 import { AuthProvider } from './context/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
